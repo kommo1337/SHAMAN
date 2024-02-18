3f8f3eee999fa37d9a63451cf8af6d61af744145
kommo <kommo@DESKTOP-Q9BEC2K>
2024-02-18 23:01:49 +0300

    SDF

--- DemoEKZA/WindowFolder/ModerWindow.xaml.cs
+++ DemoEKZA/WindowFolder/ModerWindow.xaml.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using DemoEKZA.DataFolder;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -22,6 +25,49 @@ namespace DemoEKZA.WindowFolder
         public ModerWindow()
         {
             InitializeComponent();
+            DgUser.ItemsSource = DBEntities.GetContext().User
+                    .ToList().OrderBy(u => u.FIO);
+            SurnameTB.ItemsSource = DBEntities.GetContext()
+               .User.ToList();
+            EventCb.ItemsSource = DBEntities.GetContext()
+               .Event.ToList();
+
+            UpdateRowCountLabel();
+        }
+        private void UpdateRowCountLabel()
+        {
+            
+            ColvoLabel.Content = "Количество: " + DgUser.Items.Count;
+        }
+        private void SearchBTN_Click(object sender, RoutedEventArgs e)
+        {
+            using (var context = DBEntities.GetContext())
+            {
+                
+                string selectedSurname = (SurnameTB.SelectedItem as User)?.FIO;
+                string selectedEvent = (EventCb.SelectedItem as Event)?.EventName;
+
+                
+                var query = context.User.AsQueryable();
+
+                
+                if (!string.IsNullOrEmpty(selectedSurname))
+                    query = query.Where(user => user.FIO == selectedSurname);
+
+                
+                int? selectedEventId = context.Event.FirstOrDefault(ev => ev.EventName == selectedEvent)?.IdEvent;
+
+                
+                if (selectedEventId != null)
+                {
+                    query = query.Where(user => user.IdEvent == selectedEventId);
+                }
+
+                var filteredUsers = query.ToList();
+
+                
+                DgUser.ItemsSource = filteredUsers;
+            }
         }
     }
 }
--- DemoEKZA/WindowFolder/ProfileWindow.xaml.cs
+++ DemoEKZA/WindowFolder/ProfileWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DemoEKZA.DataFolder;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -19,9 +20,43 @@ namespace DemoEKZA.WindowFolder
     /// </summary>
     public partial class ProfileWindow : Window
     {
+       
         public ProfileWindow()
         {
             InitializeComponent();
+            FillTextBoxesFromUserTable(FullName, Gender, DateOfBirth, IdNumber, Country, PhoneNumber, Email);
+        }
+
+        public static void FillTextBoxesFromUserTable(TextBox fullNameTextBox, TextBox genderTextBox, TextBox dateOfBirthTextBox, TextBox idNumberTextBox, TextBox countryTextBox, TextBox phoneNumberTextBox, TextBox emailTextBox)
+        {
+            try
+            {
+                using (var context = DBEntities.GetContext())
+                {
+                   
+                    var user = context.User.FirstOrDefault();
+
+                    if (user != null)
+                    {
+                        
+                        fullNameTextBox.Text = user.FIO;
+                        genderTextBox.Text = Convert.ToString(user.IdGender);
+                        dateOfBirthTextBox.Text = DateTime.Now.ToString();
+                        idNumberTextBox.Text = Convert.ToString( user.IdUser);
+                        countryTextBox.Text = "Russia";
+                        phoneNumberTextBox.Text = user.Phone;
+                        emailTextBox.Text = user.Email;
+                    }
+                    else
+                    {
+                        MessageBox.Show("Пользователь не найден.");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Ошибка при заполнении текстбоксов: {ex.Message}");
+            }
         }
     }
 }
